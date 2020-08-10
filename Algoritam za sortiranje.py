d7b1d707d8f2ee90bb5190d3685997cbc59104f9
LukaMihajlovic2004 <69327697+LukaMihajlovic2004@users.noreply.github.com>
2020-08-10 23:43:34 +0200

    Update Algoritam za sortiranje.py

--- Algoritam za sortiranje.py
+++ Algoritam za sortiranje.py
@@ -1,27 +1,40 @@
 import pygame as pg
-import random
+import math, random
 pg.init()
 
+#promenljive
 (sirina, visina) = (1080, 540)
-global sirinasred
-global visinasred
-(sirinasred, visinasred) = (int(sirina / 2), int(visina / 2))
-    
 prozor = pg.display.set_mode((sirina,visina))
 prozor.fill((255,255,255))
 
 global dugmesir
 global dugmevis
-(dugmesir, dugmevis) = (300, 150)
+(dugmesir, dugmevis) = (150, 75)
 
-pg.draw.rect(prozor,(50,50,200),(int(sirinasred - dugmesir), int(visinasred - dugmevis), int(dugmesir) , int(dugmevis)))
+(sirinasred, visinasred) = (int(sirina / 2), int(visina / 2))
 
-pg.display.update()
+#dimenzije i podaci za bubble sort dugme
+global bubblecenter
+global bubblesirina
+global bubblevisina
+bubblecenter = (142, 104)
+bubblesirina = int(sirinasred / 8)
+bubblevisina = int(visinasred / 4)
 
-def bubblesort():
+#slova i unos textbox
+global font
+font = pg.font.Font(None,30)
+unostekst = ''
+
+#crtanje
+pg.draw.rect(prozor,(50,50,200),(int(bubblesirina), int(bubblevisina), int(dugmesir) , int(dugmevis)))
+prozor.blit((font.render(("Broj elemenata:"), True, (0,0,0))),(25,475))
+prozor.blit((font.render(("Bubble sort"), True, (255,255,255))),(bubblecenter[0] - dugmesir / 2.5, bubblecenter[1] - dugmevis / 8))
+
+def bubblesort(element):
     #kreiranje liste i random elemenata
-    brojelemenata = int(input("Koliko elemenata imamo?")) #unos koliko elemenata
-    lst = [] 
+    brojelemenata = int(element)
+    lst = []
 
     for i in range(0,brojelemenata,1):
         rand = random.randint(10,500) #random od 10 do 500
@@ -49,21 +62,28 @@ def bubblesort():
 #glavna funkcija za dugme:
 
 def dugme():
-    inbox = False #inbox - proverava da li je mis unutar dugmeta
-    event = pg.event.wait()
+    global unostekst
+    global tekstrender
+    tekstrender = font.render(unostekst, True, (0,0,0))
+    prozor.blit(tekstrender,(25,500))
+    element = unostekst
     
+    event = pg.event.wait()
+    if event.type == pg.KEYDOWN:
+            unostekst += event.unicode
+        
     if event.type == pg.MOUSEMOTION or event.type == pg.MOUSEBUTTONDOWN:
         pozmisa = pg.mouse.get_pos() #kada se klikne ili pomeri mis pronadje se pozicija
         
-        if sirinasred > pozmisa[0] > sirinasred - dugmesir and visinasred > pozmisa[1] > visinasred - dugmevis: #provera da li je mis u dugmetu
-            inbox = True
-        else:
-            inbox = False
+        print(pozmisa[0],bubblevisina,(bubblecenter[0]+bubblesirina/2),(bubblecenter[0]-bubblesirina))
+        if event.type == pg.MOUSEBUTTONDOWN:
             
-    if event.type == pg.MOUSEBUTTONDOWN and inbox == True: #ako je u dugmetu i klikne se, izvrsi se bubble sort
-        bubblesort()
+            if ((pozmisa[0] < bubblecenter[0] + dugmesir / 2) and (pozmisa[0] > bubblecenter[0] - dugmesir / 2)) and ((pozmisa[1] < bubblecenter[1] + dugmevis / 2) and (pozmisa[1] > bubblecenter[1] - dugmevis / 2)):
+                bubblesort(element)
+
 while True:
     pg.time.wait(31)
+    pg.display.update()
     dugme()
 
 
